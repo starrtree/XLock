e9d40327e793274d87aee8d983ac0f440faa6b0e
starrtree <max@starrtree.org>
2026-09-26 02:39:42 -0400

    Load premium resource-driven surface system

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './styles.css'
+import './premium-surfaces.css'
 
 class XLockErrorBoundary extends React.Component {
   constructor(props) {
